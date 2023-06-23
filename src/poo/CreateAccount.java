f2d5bd2c65d094ad3a294639456628307be642e7
Jose SR <josesanchez09190@gmail.com>
2023-06-23 01:04:50 -0600

    add concepts of POO

--- src/poo/CreateAccount.java
+++ src/poo/CreateAccount.java
@@ -1,16 +1,16 @@
 package poo;
 
-import poo.proyecto_bytebank.Account;
+import poo.project_bytebank.Account;
 
 public class CreateAccount {
     public static void main(String[] args) {
 
-        Account account = new Account();
-        account.balance = 1000;
-        System.out.println(account.balance);
+        Account account = new Account(3443);
+        account.deposit((double )1000);
+        System.out.println(account.getBalance());
 
-        Account account2 = new Account();
-        account2.balance = 1500;
-        System.out.println(account2.balance);
+        Account account2 = new Account(2342);
+        account2.deposit((double) 1500);
+        System.out.println(account2.getBalance());
     }
 }
---
+++ src/poo/project_bytebank/TestAccess.java
@@ -0,0 +1,10 @@
+package poo.project_bytebank;
+
+public class TestAccess {
+    public static void main(String[] args) {
+        Account account = new Account(2342);
+        account.deposit(300.0);
+        account.withdrawAccountBalance(200.0);
+        System.out.println("Balance: " + account.getBalance());
+    }
+}
