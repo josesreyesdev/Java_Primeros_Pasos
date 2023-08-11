f013104b4caa4235adc2a359886ce21b61805817
Jose SR <josesanchez09190@gmail.com>
2023-08-11 16:36:15 -0600

    refactor natural order class

--- src/ColeccionesWrappersLambdaExpressions/order_list/NaturalOrder.java
+++ src/ColeccionesWrappersLambdaExpressions/order_list/NaturalOrder.java
@@ -1,5 +1,6 @@
 package ColeccionesWrappersLambdaExpressions.order_list;
 
+import org.jetbrains.annotations.NotNull;
 import poo.bytebank_inheritance.model.Account;
 import poo.bytebank_inheritance.model.CheckingAccount;
 
@@ -8,23 +9,7 @@ import java.util.*;
 public class NaturalOrder {
     public static void main(String[] args) {
 
-        Account account1 = new CheckingAccount(19, 33);
-        account1.deposit(333.0);
-
-        Account account2 = new CheckingAccount(20, 44);
-        account2.deposit(444.0);
-
-        Account account3 = new CheckingAccount(2, 11);
-        account3.deposit(111.0);
-
-        Account account4 = new CheckingAccount(3, 22);
-        account4.deposit(222.0);
-
-        List<Account> list = new ArrayList<>();
-        list.add(account1);
-        list.add(account2);
-        list.add(account3);
-        list.add(account4);
+        List<Account> list = getAccountList();
         for (Account ele: list) {
             System.out.println("List: "+ ele);
         }
@@ -60,6 +45,28 @@ public class NaturalOrder {
         System.out.println("Numbers "+ Arrays.toString(numbers));
         Arrays.sort(numbers);
         System.out.println("Numbers "+ Arrays.toString(numbers));
+    }
+
+    @NotNull
+    private static List<Account> getAccountList() {
+        Account account1 = new CheckingAccount(19, 33);
+        account1.deposit(333.0);
+
+        Account account2 = new CheckingAccount(20, 44);
+        account2.deposit(444.0);
+
+        Account account3 = new CheckingAccount(2, 11);
+        account3.deposit(111.0);
+
+        Account account4 = new CheckingAccount(3, 22);
+        account4.deposit(222.0);
+
+        List<Account> list = new ArrayList<>();
+        list.add(account1);
+        list.add(account2);
+        list.add(account3);
+        list.add(account4);
+        return list;
     }
 }
 
